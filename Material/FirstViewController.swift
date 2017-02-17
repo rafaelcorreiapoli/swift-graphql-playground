3500525ab47aa6972c4018132b82dd87d496d849
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-02-16 22:12:28 -0200

    Initial Commit

---
+++ Material/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Material
+//
+//  Created by Rafael Ribeiro Correia on 2/16/17.
+//  Copyright © 2017 Rafael Ribeiro Correia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
